8c3849d56b24e80970d72db5bfd83b4dbca88cd1
Drew Wagner <drew.wagner@mail.concordia.ca>
2019-08-04 12:05:48 -0400

    Added documentation to GameManager

--- Assets/Version 1/Scripts/GameManager.cs
+++ Assets/Version 1/Scripts/GameManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+/// <summary>
+/// Manages global game mechanics: UI, cursor locking, and eventually other things.
+/// </summary>
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
     public Text debugInfo;
-    public Light sun;
-    public float waterLevel = 0;
 
     Camera mainCamera;
 
@@ -19,28 +20,31 @@ public class GameManager : MonoBehaviour
         instance = this;
         mainCamera = Camera.main;
     }
-    // Start is called before the first frame update
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if (Time.frameCount % 30 == 0) System.GC.Collect();
+        // Returns to Main menu when Escape key is pressed.
         if (!Application.isEditor && Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
             SceneManager.LoadScene("MainMenu");
         }
+
+        // Toggles display of debug info
         if (Input.GetKeyDown(KeyCode.F1))
         {
             debugInfo.gameObject.SetActive(!debugInfo.gameObject.activeSelf);
         }
-        if (debugInfo.gameObject.activeSelf)
+
+        // Updates debug info information every 15 frames
+        if (debugInfo.gameObject.activeSelf && Time.frameCount%15 == 0)
         {
             int fps = Mathf.RoundToInt(1f / Time.deltaTime);
             debugInfo.text = $"FPS: {fps}\nPOS: {mainCamera.transform.position.ToString()}\nFOR: {mainCamera.transform.forward.ToString()}";
